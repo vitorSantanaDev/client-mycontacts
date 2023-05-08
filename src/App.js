3342a25b510bb26c49d2cdd4c47dd747d8ca800b
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-05-08 20:03:10 -0300

    :tada: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './assets/styles/global'
+import { ThemeProvider } from 'styled-components'
+import defaultTheme from './assets/styles/themes/default'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyles />
+			<h1>React App</h1>
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App
---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,37 @@
+export default {
+	grid: {
+		container: '50rem',
+		gutter: '3.2rem'
+	},
+	border: {
+		radius: '0.4rem'
+	},
+	font: {
+		family: "'Sora', sans-serif",
+		bold: 600,
+		light: 300,
+		normal: 400,
+		sizes: {
+			xsmall: '1.2rem',
+			small: '1.4rem',
+			medium: '1.6rem',
+			large: '1.8rem',
+			xlarge: '2.0rem',
+			xxlarge: '2.8rem',
+			huge: '5.2rem'
+		}
+	},
+	colors: {
+		mainBg: '#F5F6Fc',
+		primary: '#5061FC'
+	},
+	spacings: {
+		xxsmall: '0.8rem',
+		xsmall: '1.6rem',
+		small: '2.4rem',
+		medium: '3.2rem',
+		large: '4.0rem',
+		xlarge: '4.8rem',
+		xxlarge: '5.6rem'
+	}
+}

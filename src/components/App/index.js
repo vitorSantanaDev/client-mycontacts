cc102f56e40b1587455b29366a4a3725aff05980
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-05-09 20:52:13 -0300

    feat: adding application initial routing structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,25 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from '../../assets/styles/global'
 import defaultTheme from '../../assets/styles/themes/default'
 
 import Header from '../Header'
+import Routes from '../../Routes'
+
 import { Container } from './styles'
-import ContactsList from '../ContactsList'
 
 function App() {
 	return (
-		<ThemeProvider theme={defaultTheme}>
-			<GlobalStyles />
-			<Container>
-				<Header />
-				<ContactsList />
-			</Container>
-		</ThemeProvider>
+		<BrowserRouter>
+			<ThemeProvider theme={defaultTheme}>
+				<GlobalStyles />
+				<Container>
+					<Header />
+					<Routes />
+				</Container>
+			</ThemeProvider>
+		</BrowserRouter>
 	)
 }
 

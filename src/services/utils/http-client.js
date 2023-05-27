8344a744610a2056eac58cb3001935923d0f91bb
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-05-26 21:36:55 -0300

    refactor: refactoring Http client class

--- src/services/utils/http-client.js
+++ src/services/utils/http-client.js
@@ -6,24 +6,54 @@ class HttpClient {
 	constructor(baseUrl) {
 		this.baseUrl = baseUrl
 	}
-	async get(path) {
-		await delay(500)
 
-		const response = await fetch(`${this.baseUrl}${path}`)
+	async makeRequest(path, options) {
+		await delay(1500)
 
-		let body = null
+		const headers = new Headers()
+
+		if (options.body) headers.append('Content-Type', 'application/json')
+
+		if (options.headers) {
+			Object.entries(options.headers).forEach(([key, value]) => {
+				headers.append(key, value)
+			})
+		}
+
+		const response = await fetch(`${this.baseUrl}${path}`, {
+			method: options.method,
+			body: JSON.stringify(options.body),
+			headers
+		})
+
+		let responseBody = null
 
 		const contentType = response.headers.get('Content-Type')
 
 		if (contentType.includes('application/json')) {
-			body = await response.json()
+			responseBody = await response.json()
 		}
 
 		if (response.ok) {
-			return body
+			return responseBody
 		}
 
-		throw new APIError(response, body)
+		throw new APIError(response, responseBody)
+	}
+
+	get(path, options) {
+		return this.makeRequest(path, {
+			method: 'GET',
+			headers: options?.headers
+		})
+	}
+
+	post(path, options) {
+		return this.makeRequest(path, {
+			method: 'POST',
+			body: options?.body,
+			headers: options?.headers
+		})
 	}
 }
 
